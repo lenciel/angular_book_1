52193729f8f685f1ed1364a6206ec42fd4f75c86
lenciel <lenciel@gmail.com>
2015-06-27 22:57:12 +0800

    add chap2/sample1
    without instance of controller

---
+++ chap2/sample1/app.js
@@ -0,0 +1,9 @@
+angular.module('myApp', []).controller('GreetingController', function($scope){
+	$scope.now = new Date();
+	$scope.helloMessages = ['Hello', 'Bonjour', 'Hloa', 'Ciao', 'Hallo'];
+	$scope.greeting = $scope.helloMessages[0];
+
+	$scope.getRandomHelloMessage = function() {
+		$scope.greeting = $scope.helloMessages[parseInt(Math.random() * $scope.helloMessages.length)];
+	}
+});
